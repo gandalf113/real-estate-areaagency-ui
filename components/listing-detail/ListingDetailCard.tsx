6bcfdf6f18924d959c8d8a07ddcf780bd4e7b882
Aleksander Szczepaniak <aleksander.szczepaniak113@gmail.com>
2024-07-16 09:44:16 +0200

    FIX div inside p

--- components/listing-detail/ListingDetailCard.tsx
+++ components/listing-detail/ListingDetailCard.tsx
@@ -7,10 +7,10 @@ const ListingDetailCard = (props: ListingDetailCardProps) => {
     const { name, value } = props;
 
     return (
-        <p className={`flex-col items-center text-center shadow bg-zinc-50 bg-opacity-45 text-gray-900 p-4 rounded w-full`}>
+        <div className={`flex-col items-center text-center shadow bg-zinc-50 bg-opacity-45 text-gray-900 p-4 rounded w-full`}>
             <div className={`mb-1`}>{name}</div>
             <div className={`text-2xl`}>{value}</div>
-        </p>
+        </div>
     )
 }
 
