15341fc5bd13483e55cccb1e78d1af701967089f
Aleksander Szczepaniak <aleksander.szczepaniak113@gmail.com>
2024-07-18 10:12:36 +0200

    FIX pagination not fitting on mobile

--- components/filters/Pagination.tsx
+++ components/filters/Pagination.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React from 'react';
-import {useRouter, useSearchParams} from "next/navigation";
+import React, { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from "next/navigation";
 import useTranslations from "@/components/hooks/useTranslations";
 
 interface PaginationProps {
@@ -9,10 +9,31 @@ interface PaginationProps {
     totalPages: number;
 }
 
-const Pagination = ({currentPage, totalPages}: PaginationProps) => {
+const Pagination = ({ currentPage, totalPages }: PaginationProps) => {
     const router = useRouter();
     const searchParams = useSearchParams();
-    const { nextPage: nextPageStr} = useTranslations();
+    const { nextPage: nextPageStr } = useTranslations();
+
+    const [maxDisplayedPages, setMaxDisplayedPages] = useState(5);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 600) {
+                setMaxDisplayedPages(2);
+            } else if (window.innerWidth < 768) {
+                setMaxDisplayedPages(3);
+            } else {
+                setMaxDisplayedPages(6);
+            }
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const handlePageChange = (page: number) => {
         if (page > 0 && page <= totalPages) {
@@ -24,7 +45,6 @@ const Pagination = ({currentPage, totalPages}: PaginationProps) => {
 
     const renderPageNumbers = () => {
         const pageNumbers = [];
-        const maxDisplayedPages = 5;
         const halfMaxDisplayedPages = Math.floor(maxDisplayedPages / 2);
         let startPage = Math.max(currentPage - halfMaxDisplayedPages, 1);
         let endPage = Math.min(currentPage + halfMaxDisplayedPages, totalPages);
@@ -83,40 +103,35 @@ const Pagination = ({currentPage, totalPages}: PaginationProps) => {
     };
 
     return (
-        <>
-            <div className="flex justify-between mt-4 w-full">
-                <div className={`flex items-center text-sm`}>
-                    <button
-                        onClick={() => handlePageChange(currentPage - 1)}
-                        className="px-4 py-2 mx-1 bg-gray-200 rounded-md"
-                        disabled={currentPage === 1}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4`} fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7"/>
-                        </svg>
-                    </button>
-                    <div className={`mx-2 flex items-end`}>
-                        {renderPageNumbers()}
-                    </div>
-                    <button
-                        onClick={() => handlePageChange(currentPage + 1)}
-                        className="px-4 py-2 mx-1 bg-gray-200 rounded-md"
-                        disabled={currentPage === totalPages}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4`} fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"/>
-                        </svg>
-                    </button>
+        <div className="flex flex-wrap gap-y-4 justify-between mt-4 w-full">
+            <div className={`flex items-center text-sm`}>
+                <button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    className="px-4 py-2 mx-1 bg-gray-200 rounded-md"
+                    disabled={currentPage === 1}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                    </svg>
+                </button>
+                <div className={`mx-2 flex items-end`}>
+                    {renderPageNumbers()}
                 </div>
-
-                <button onClick={() => handlePageChange(currentPage + 1)}
-                        className="px-4 py-1 mx-1 bg-gray-200 text-sm rounded-md hover:bg-blue-300 duration-200">
-                    {nextPageStr}
+                <button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    className="px-4 py-2 mx-1 bg-gray-200 rounded-md"
+                    disabled={currentPage === totalPages}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className={`h-4 w-4`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                    </svg>
                 </button>
             </div>
-        </>
+
+            <button onClick={() => handlePageChange(currentPage + 1)} className="lg:block hidden px-4 py-1 mx-1 bg-gray-200 text-sm rounded-md hover:bg-blue-300 duration-200">
+                {nextPageStr}
+            </button>
+        </div>
     );
 };
 
