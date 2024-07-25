9c6fb3a50edcae1c66dd21e08833a7e942003f1d
Aleksander Szczepaniak <aleksander.szczepaniak113@gmail.com>
2024-07-25 21:23:24 +0200

    Revert "Remove map (test)"
    This reverts commit 732cf6c7e32a068b00ab0e349536917c3242b8dd.

--- components/ListingsWithMap.tsx
+++ components/ListingsWithMap.tsx
@@ -3,7 +3,8 @@
 import {IListing, IListingPin, LanguageType} from "@/types";
 import ListingCard from "@/components/ListingCard";
 import Pagination from "@/components/filters/Pagination";
-import {useState} from "react";
+import {useMemo, useState, useEffect} from "react";
+import dynamic from "next/dynamic";
 import useTranslations from "@/components/hooks/useTranslations";
 import Filters from "@/components/filters/Filters";
 import {useSearchParams} from "next/navigation";
@@ -18,6 +19,14 @@ interface ListingsWithMapProps {
 
 const ListingsWithMap = ({listings, pins, totalPages, currentPage, lang}: ListingsWithMapProps) => {
     const [activeLocationId, setActiveLocationId] = useState<number>();
+
+    const Map = useMemo(() => dynamic(
+        () => import('@/components/Map'),
+        {
+            loading: () => <div className={`w-full h-screen bg-gray-200 animate-pulse`}/>,
+            ssr: false
+        }
+    ), [])
 
     const handleClickPin = (locationId: number) => {
         setActiveLocationId(locationId);
@@ -65,6 +74,11 @@ const ListingsWithMap = ({listings, pins, totalPages, currentPage, lang}: Listin
                 />}
             </div>
 
+            <div className={`lg:block hidden sticky right-0 h-[calc(100vh-64px)] top-0 w-5/12`}>
+                <Map position={[52.247463, 21.015801]} zoom={10} locations={pins}
+                     activeLocationId={activeLocationId}
+                     setActiveLocationId={setActiveLocationId}/>
+            </div>
         </div>
     );
 }
