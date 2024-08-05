327d352559d05192bcdce85910c175791b2ddee2
Aleksander Szczepaniak <aleksander.szczepaniak113@gmail.com>
2024-08-05 14:40:54 +0200

    DEBUG action logs

--- app/actions.ts
+++ app/actions.ts
@@ -10,32 +10,52 @@ export async function submitContactForm(props: {
 }, link: string) {
     const { crmId, provider, name, email, phoneNumber, message } = props;
 
+    console.log('submitContactForm called with:', props, link);
+
     const text = `ID: ${crmId}\nCRM: ${provider}\nOgłoszenie: ${link}\n\nImię: ${name}\nEmail: ${email}\nTelefon: ${phoneNumber}\n\nWiadomość: ${message}`;
-    const body = {             chat_id: process.env.TELEGRAM_CHAT_ID, text: text}
+    const body = { chat_id: process.env.TELEGRAM_CHAT_ID, text: text }
 
-    const response = await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
+    try {
+        const response = await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            console.log('Telegram message sent successfully');
+            return { success: true };
+        } else {
+            console.error('Error sending Telegram message:', await response.text());
+            return { success: false };
         }
-    });
-
-    if (response.ok) {
-        return { success: true };
-    } else {
+    } catch (error) {
+        console.error('Error in submitContactForm:', error);
         return { success: false };
     }
 }
 
 export async function fetchListingImages(id: number) {
     const url = process.env.API_BASE_URL + '/listings/' + id + '/images';
-    const res = await fetch(url, {next: {revalidate: 900}})
 
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
+    console.log('fetchListingImages called with ID:', id);
+
+    try {
+        const res = await fetch(url, { next: { revalidate: 900 } });
+
+        if (!res.ok) {
+            const errorText = await res.text();
+            console.error('Error fetching listing images:', errorText);
+            throw new Error('Failed to fetch data');
+        }
+
+        const json = await res.json();
+        console.log('Fetched listing images successfully:', json.images);
+        return json.images;
+    } catch (error) {
+        console.error('Error in fetchListingImages:', error);
+        throw new Error('Failed to fetch data');
     }
-
-    const json = await res.json();
-    return json.images;
 }
