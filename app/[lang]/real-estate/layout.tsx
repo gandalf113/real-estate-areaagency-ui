b47530be4013849bdf760a58513a76d6f3627fc5
Aleksander Szczepaniak <aleksander.szczepaniak113@gmail.com>
2024-10-06 13:55:16 +0200

    CHORE add clarity script

--- app/[lang]/real-estate/layout.tsx
+++ app/[lang]/real-estate/layout.tsx
@@ -43,6 +43,14 @@ export default function RootLayout({
             `}
         </Script>
 
+        <Script id="clarity-init" type="text/javascript">
+            {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "oc8aqlz9f7");`}
+        </Script>
+
         {/* Google Tag Manager (noscript) */}
         <noscript>
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDF59VZK" height="0" width="0"
