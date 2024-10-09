47ebfe9b9093be5c8540ef5aac5a6d62f1cee867
Aleksander Szczepaniak <aleksander.szczepaniak113@gmail.com>
2024-10-09 09:06:24 +0200

    CHORE add meta pixel

--- app/[lang]/real-estate/layout.tsx
+++ app/[lang]/real-estate/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({
             `}
         </Script>
 
+        {/* Clarity Analytics */}
         <Script id="clarity-init" type="text/javascript">
             {`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
@@ -50,6 +51,23 @@ export default function RootLayout({
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "oc8aqlz9f7");`}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel-init" type="text/javascript">
+            {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '8261155657315177');
+            fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+            <img height="1" width="1" className="hidden" src="https://www.facebook.com/tr?id=8261155657315177&ev=PageView&noscript=1"
+        /></noscript>
 
         {/* Google Tag Manager (noscript) */}
         <noscript>
